9e9c8128b4873de23826ef1dc8bca722cdb191f1
Kyriri <736dccb6@opayq.com>
2020-08-19 19:22:44 -0300

    Render cards from an array

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-document.querySelector('p').innerHTML = 'Ai! laurië lantar lassi súrinen,<BR>yéni unótimë ve rámar aldaron!';
+document.getElementsByTagName('p')[1].innerHTML = 'Galadriel';
+
+const catalog = sample;
+
+function createCard(index) {
+  return `
+  <div class='card'>
+  <h2>${catalog[index].title}</h2>
+  <p>by: ${catalog[index].author}</p>
+  <p>Original language: ${catalog[index].language}</p>
+  </div>`;
+}
+
+function renderCards() {
+  for (let i = 0; i < catalog.length; i += 1)
+    document.getElementById('card-box').innerHTML += createCard(i);
+}
+
+renderCards();
---
+++ sample-library.js
@@ -0,0 +1,71 @@
+document.getElementsByTagName('p')[0].innerHTML = 'Ai! laurië lantar lassi súrinen,<BR>yéni unótimë ve rámar aldaron!';
+
+// sample library for development purposes
+
+const sample = [
+  {
+    id: 1,
+    title: `Colorless Tsukuru Tazaki and his years of pilgrimage`,
+    author: `Haruki Murakami`, 
+    language: `Japanese`, 
+    category: `holiday read`,
+    comment: `That book I saw at the Bali hostel, and then at the Bangkok hostel`,
+    quotes: `'It's a fascinating story, but hard to believe,' Haida admitted. 
+    'Because there's no logical explanation?'
+    'Exactly.'
+    'No way to prove it.'
+    'The only way you know if it's real or not, the only way to prove it, is by actually making the deal. Isn't that how it works?'
+    Midorikawa nodded. 'Exactly. Unless you take the leap, you can't prove it. And once you actually make the leap, there's no need to prove it anymore. There's no middle ground.(...)'
+    
+    The chair was a simple Scandinavian design of chrome and white leather. Beautiful, clean, and silent, with not an ounce of warmth, like fine rain falling under the midnight sun.`,
+    read: true,
+  },{
+    id: 2,
+    title: `The Catcher in the Rye`,
+    author: ``, 
+    language: `English`, 
+    category: `classics`,
+    comment: ``,
+    quotes: `"I'm not trying to tell you", he said, "that only educated and scholarly men are able to contribute something valuable to the world. It's not so. But I do say that educated and scholarly men, if they're brilliant and creative to begin with — which, unfortunately, is rarely the case — tend to leave infinitely more valuable records behind them than men do who are _merely_ brilliant and creative. They tend to express themselves more clearly, and they usually have a passion for following their thoughts through to the end. (...)`,
+    read: true,
+  },{
+    id: 3,
+    title: `La Tregua`,
+    author: `Mario Benedetti`, 
+    language: `Spanish`, 
+    category: `holiday read`,
+    comment: `From Fabi's bookshelf`,
+    quotes: `Blanca tiene por lo menos algo de común conmigo: también es una triste con vocación de alegre`,
+    read: true,
+  },{
+    id: 6,
+    title: `Think like a programmer: An introduction to creative problem solving`,
+    author: `Anton Spraul`, 
+    language: `English`, 
+    category: `technical`,
+    comment: `Recommended by The Odin Project`,
+    quotes: ``,
+    read: false,
+  },{
+    id: 11,
+    title: `The biggest bluff`,
+    author: `Maria Konnikova`, 
+    language: `English`, 
+    category: `non-fiction`,
+    comment: `The journalist who became a poker champion (NYT)`,
+    quotes: ``,
+    read: false,
+  }
+]
+
+/*
+},{
+id: ,
+title: ``,
+author: ``, 
+language: ``, 
+category: ``,
+comment: ``,
+quotes: ``,
+read: ,
+*/
